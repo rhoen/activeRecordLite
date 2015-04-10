92c567ace78bb20ab37648d8c0bc0f5fd3b3d2ab
= <=>
2015-04-10 16:52:43 -0400

    write where method on searchable, still buggy

--- active_record_lite_skeleton/lib/02_searchable.rb
+++ active_record_lite_skeleton/lib/02_searchable.rb
@@ -3,10 +3,23 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map do |key, value|
+      "#{key} = ?"
+    end.join(' AND ')
+
+    sql_statement = <<-SQL
+      SELECT
+        *
+      FROM
+        #{self.class.to_s.downcase}
+      WHERE
+        #{where_line}
+    SQL
+
+    DBConnection.execute(sql_statement, params.values)
   end
 end
 
 class SQLObject
-  include Searcheable
+  extend Searchable
 end
