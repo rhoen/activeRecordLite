4426c2f0ca588f4f043a1a014c0746ec05ef3cae
= <=>
2015-04-10 11:22:04 -0400

    complete my_attr_accessor class method

--- active_record_lite_skeleton/lib/00_attr_accessor_object.rb
+++ active_record_lite_skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+
+    names.each do |name|
+      define_method(name.to_sym) do
+        self.instance_variable_get("@#{name.to_sym}")
+      end
+      define_method("#{name.to_sym}=") do |arg|
+        self.instance_variable_set("@#{name.to_sym}", arg)
+      end
+    end
   end
 end
